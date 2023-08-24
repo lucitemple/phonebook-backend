d32eb4f10cc5dba2a9bc35e51444a4af4ee1f2bd
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2023-08-24 17:36:59 +1000

    Create express server with REST api

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+// For logging requests
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+app.use(express.json());
+
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${
+      persons.length
+    } people.</p><p>${new Date()}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  person ? response.json(person) : response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const genRandomNumber = () => Math.floor(Math.random() * 999999999999);
+const generateId = () => {
+  let id = genRandomNumber();
+  persons.map((p) => {
+    if (p.id === id) {
+      id = genRandomNumber();
+    }
+  });
+
+  return id;
+};
+
+app.post(
+    "/api/persons",   
+    morgan(":method :url :status :res[content-length] - :response-time ms :body"), (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "name or number is missing." });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response
+      .status(400)
+      .json({ error: "name already exists in phonebook. name must be unique" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT} at http://localhost:${PORT}`);
