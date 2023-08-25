5af346548cf64a91852c97f5f4fb9841eb789f30
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2023-08-25 17:45:37 +1000

    configure for use with frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 morgan.token("body", (req) => JSON.stringify(req.body));
 
 app.use(express.json());
-
+app.use(cors());
 
 let persons = [
   {
@@ -32,35 +33,74 @@ let persons = [
   },
 ];
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
-});
+app.get(
+  "/",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    response.send("<h1>Hello World</h1>");
+  }
+);
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    } people.</p><p>${new Date()}</p>`
-  );
-});
+app.get(
+  "/info",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    response.send(
+      `<p>Phonebook has info for ${
+        persons.length
+      } people.</p><p>${new Date()}</p>`
+    );
+  }
+);
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+app.get(
+  "/api/persons",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    response.json(persons);
+  }
+);
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get(
+  "/api/persons/:id",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
 
-  person ? response.json(person) : response.status(404).end();
-});
+    person ? response.json(person) : response.status(404).end();
+  }
+);
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+app.delete(
+  "/api/persons/:id",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    const id = Number(request.params.id);
 
-  response.status(204).end();
-});
+    persons = persons.filter((person) => Number(person.id) !== id);
+
+    response.status(204).end();
+  }
+);
+
+app.put(
+  "/api/persons/:id",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    const id = Number(request.params.id);
+    const { name, number } = request.body;
+    const updatedPerson = {
+      name,
+      number,
+      id,
+    };
+    persons = persons.map((person) =>
+      person.id === id ? updatedPerson : person
+    );
+    response.json(updatedPerson);
+  }
+);
 
 const genRandomNumber = () => Math.floor(Math.random() * 999999999999);
 const generateId = () => {
@@ -75,30 +115,32 @@ const generateId = () => {
 };
 
 app.post(
-    "/api/persons",   
-    morgan(":method :url :status :res[content-length] - :response-time ms :body"), (request, response) => {
-  const body = request.body;
+  "/api/persons",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response) => {
+    const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ error: "name or number is missing." });
+    if (!body.name || !body.number) {
+      return response.status(400).json({ error: "name or number is missing." });
+    }
+
+    if (persons.find((person) => person.name === body.name)) {
+      return response.status(400).json({
+        error: "name already exists in phonebook. name must be unique",
+      });
+    }
+
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
   }
-
-  if (persons.find((person) => person.name === body.name)) {
-    return response
-      .status(400)
-      .json({ error: "name already exists in phonebook. name must be unique" });
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
-
-  persons = persons.concat(person);
-
-  response.json(person);
-});
+);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
